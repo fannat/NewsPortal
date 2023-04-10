23c8a5776f570da549a567bf3fb72ee68ae10ac8
Levon Asatrian <lyov.asatrian@gmail.com>
2023-04-11 03:29:50 +0400

    Yet another commit. Trying to add some HTML

--- newspaper/news/views.py
+++ newspaper/news/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'news/post_list.html', {'posts': posts})
+
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id)
+    return render(request, 'news/post_detail.html', {'post': post})
--- newspaper/newspaper/urls.py
+++ newspaper/newspaper/urls.py
@@ -14,9 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 
+
+from django.contrib import admin
+from django.urls import path, include
+from .views import post_list, post_detail
+
+app_name = 'news'
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', post_list, name='post_list'),
+    path('<int:id>/', post_detail, name='post_detail'),
 ]
